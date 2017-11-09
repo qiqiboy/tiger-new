9aadacc77d59230ea7a1d4708719b3ad944f18a0
qiqiboy <imqiqiboy@gmail.com>
2017-11-09 12:16:00 +0800

    more polyfills

--- template/scripts/config/polyfills.js
+++ template/scripts/config/polyfills.js
@@ -6,8 +6,11 @@ require('raf-dom').polyfill();
 
 // ES6
 require('core-js/modules/es6.object.assign');
+require('core-js/modules/es6.object.create');
 require('core-js/es6/promise');
 require('core-js/es6/map');
 require('core-js/es6/set');
 require('core-js/es6/array');
 require('core-js/es6/string');
+
+require('core-js/modules/es7.object.values');
