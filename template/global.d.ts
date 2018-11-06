154bacc00492018d67f0b3f40f4f24f102651e66
qiqiboy <imqiqiboy@gmail.com>
2018-11-06 11:22:07 +0800

    feat: 增加默认全局类型声明辅助方法

--- template/global.d.ts
+++ template/global.d.ts
@@ -6,3 +6,14 @@ declare module '*.jpeg'
 declare module '*.gif'
 declare module '*.bmp'
 declare module '*.tiff'
+
+/**
+ * From T delete a set of properties P
+ */
+type Omit<T, P> = Pick<T, Exclude<keyof T, P>>;
+
+/**
+ * create HOC(Higher Order Component)
+ *
+ */
+type HOC<InjectProps> = <Self>(Component: React.ComponentType<Self & InjectProps>) => React.ComponentType<Self>;
