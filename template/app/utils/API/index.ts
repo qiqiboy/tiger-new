e41334cef90787f16858ade0e8c6a4f08d7527d1
qiqiboy <imqiqiboy@gmail.com>
2018-11-07 20:08:43 +0800

    refactor(template): add declaration for API

--- template/app/utils/API/index.ts
+++ template/app/utils/API/index.ts
@@ -1,19 +1,34 @@
 import path from 'path';
 
+// 导入接口定义
+import { API as PORTAL } from './apis/PORTAL';
+
+type API<T> = { [P in keyof T]: T[P] extends object ? API<T[P]> : (...args: Array<string | number>) => string };
+
+interface IAPI {
+    // 这里需要传递接口结构声明
+    PORTAL: typeof PORTAL;
+}
+
 const isDev = process.env.NODE_ENV === 'development';
-const apiCtx = require.context('./apis', false, /\.js$/);
+// @ts-ignore
+const apiCtx = require.context('./apis', false, /\.[jt]sx?$/);
 
 export default apiCtx.keys().reduce((exports, file) => {
-    exports[path.basename(file, '.js')] = enhanced(apiCtx(file));
+    exports[path.basename(file, path.extname(file))] = enhanced(apiCtx(file));
 
     return exports;
-}, {});
+}, {}) as API<
+    IAPI & {
+        [key: string]: any;
+    }
+>;
 
 function enhanced(config) {
     const host = isDev ? config.HOST[0] : config.HOST[1];
-    const createAPI = API =>
-        Object.keys(API).reduce((result, key) => {
-            const pathname = API[key];
+    const createAPI = apis =>
+        Object.keys(apis).reduce((result, key) => {
+            const pathname = apis[key];
 
             result[key] =
                 typeof pathname === 'string'
