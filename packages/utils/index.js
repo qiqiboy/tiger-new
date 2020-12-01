58798435cb9c18f547eb82ef1f645aa4b4ca0439
qiqiboy <imqiqiboy@gmail.com>
2020-12-01 19:14:08 +0800

    feat: add packages/utils

---
+++ packages/utils/index.js
@@ -0,0 +1,9 @@
+const formatWebpackMessages = require('./formatWebpackMessages');
+const typescriptFormatter = require('./typescriptFormatter');
+const ForkTsCheckerWebpackPlugin = require('./ForkTsCheckerWebpackPlugin');
+
+module.exports = {
+    formatWebpackMessages,
+    typescriptFormatter,
+    ForkTsCheckerWebpackPlugin
+};
