837ddc04c7e20e69aac4131fb63ab692efe225a5
qiqiboy <imqiqiboy@gmail.com>
2021-09-09 14:44:09 +0800

    chore(utils): Exclude BASE_NAME from createEnvironmentHash

--- packages/utils/createEnvironmentHash.js
+++ packages/utils/createEnvironmentHash.js
@@ -1,9 +1,10 @@
 'use strict';
 const { createHash } = require('crypto');
+const lodash = require('lodash');
 
 module.exports = env => {
   const hash = createHash('md5');
-  hash.update(JSON.stringify(env));
+  hash.update(JSON.stringify(lodash.omit(env, 'BASE_NAME')));
 
   return hash.digest('hex');
 };
